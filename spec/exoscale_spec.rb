ae129f1b178d74587fe2c36499ad7e261446b34a
Nicolas Brechet <nicolasbrechet@me.com>
2014-11-11 14:48:51 +0100

    Added tests for collections

--- spec/exoscale_spec.rb
+++ spec/exoscale_spec.rb
@@ -1,3 +1,5 @@
+# encoding: UTF-8
+
 require "minitest/autorun"
 require "fog/exoscale"
 
@@ -5,15 +7,43 @@ describe Fog::Compute::Exoscale do
   describe "when global config is available" do
     before do
       @config = {
-        :exoscale_api_key           => "bogus_api_key",
-        :exoscale_secret_access_key => "bogus_secret_access_key"
+        :exoscale_api_key           => ENV["EXO_API_KEY"],
+        :exoscale_secret_access_key => ENV["EXO_SECRET_KEY"]
       }
 
       @service = Fog::Compute::Exoscale.new(@config)
     end
 
-    it "responds to #request" do
-      assert_respond_to @service, :request
+    it "must respond to #request" do
+      @service.must_respond_to :request
+    end
+    
+    it "must respond to collection #flavors" do
+      @service.must_respond_to :flavors
+    end
+    
+    it "must respond to collection #jobs" do
+      @service.must_respond_to :jobs
+    end
+    
+    it "must respond to collection #servers" do
+      @service.must_respond_to :servers
+    end
+    
+    it "must respond to collection #images" do
+      @service.must_respond_to :images
+    end
+    
+    it "must respond to collection #security_groups" do
+      @service.must_respond_to :security_groups
+    end
+    
+    it "must respond to collection #security_group_rules" do
+      @service.must_respond_to :security_group_rules
+    end
+    
+    it "must respond to collection #zones" do
+      @service.must_respond_to :zones
     end
   end
 

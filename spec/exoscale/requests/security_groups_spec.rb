02e647cea80d4e9ae87a93fa00c7c4136f123ebc
Nicolas Brechet <nicolasbrechet@me.com>
2014-11-12 16:19:09 +0100

    Corrected failing tests

--- spec/exoscale/requests/security_groups_spec.rb
+++ spec/exoscale/requests/security_groups_spec.rb
@@ -43,36 +43,36 @@ describe Fog::Compute::Exoscale do
   
   describe "when authorizing rules to a new security group" do
     before do
-      @new_sg = @client.create_security_group(:name => 'fog_exoscale_test_sg_rules')['createsecuritygroupresponse']['securitygroup']
+      @another_sg = @client.create_security_group(:name => "fog_exoscale_test_sg_rules_#{Random.new.rand(1..2000)}")['createsecuritygroupresponse']['securitygroup']
       # empty security group
     end
     
     it "must have no rules" do
-      @new_sg['ingressrule'].must_be_empty
-      @new_sg['egressrule'].must_be_empty
+      @another_sg['ingressrule'].must_be_empty
+      @another_sg['egressrule'].must_be_empty
     end
     
     describe "When adding new rules" do
       before do
         # adding an ingress rule:
         #  keeping the job id
-        ingress_job_id = @client.authorize_security_group_ingress(:securityGroupName => @new_sg['name'], :cidrList => '0.0.0.0/0', :startPort => '22', :endPort => '22', :protocol => 'TCP')['authorizesecuritygroupingressresponse']['jobid']
+        ingress_job_id = @client.authorize_security_group_ingress(:securityGroupName => @another_sg['name'], :cidrList => '0.0.0.0/0', :startPort => '22', :endPort => '22', :protocol => 'TCP')['authorizesecuritygroupingressresponse']['jobid']
         #  then querying the async job  
         ingress_result = @client.query_async_job_result(:jobId => ingress_job_id)['queryasyncjobresultresponse']['jobresult']
         #  and finally retrieving the rule id so we can delete it later
         @ingress_rule_id = ingress_result['securitygroup']['ingressrule'][0]['ruleid']
         
         # and same for egress rule
-        egress_job_id = @client.authorize_security_group_egress(:securityGroupName => @new_sg['name'], :cidrList => '0.0.0.0/0', :startPort => '22', :endPort => '22', :protocol => 'TCP')['authorizesecuritygroupegressresponse']['jobid']
+        egress_job_id = @client.authorize_security_group_egress(:securityGroupName => @another_sg['name'], :cidrList => '0.0.0.0/0', :startPort => '22', :endPort => '22', :protocol => 'TCP')['authorizesecuritygroupegressresponse']['jobid']
         egress_result = @client.query_async_job_result(:jobId => egress_job_id)['queryasyncjobresultresponse']['jobresult']
         @egress_rule_id = egress_result['securitygroup']['egressrule'][0]['ruleid']
         
-        @new_sg = @client.list_security_groups['listsecuritygroupsresponse']['securitygroup'].select{|sg| sg['name']==@new_sg['name']}[0]
+        @another_sg = @client.list_security_groups['listsecuritygroupsresponse']['securitygroup'].select{|sg| sg['name']==@another_sg['name']}[0]
       end
       
       it "must list new rules" do
-        @new_sg['ingressrule'][0]['ruleid'].must_equal @client.list_security_groups['listsecuritygroupsresponse']['securitygroup'].select{|sg| sg['name']==@new_sg['name']}[0]['ingressrule'][0]['ruleid']
-        @new_sg['egressrule'][0]['ruleid'].must_equal @client.list_security_groups['listsecuritygroupsresponse']['securitygroup'].select{|sg| sg['name']==@new_sg['name']}[0]['egressrule'][0]['ruleid']
+        @another_sg['ingressrule'][0]['ruleid'].must_equal @client.list_security_groups['listsecuritygroupsresponse']['securitygroup'].select{|sg| sg['name']==@another_sg['name']}[0]['ingressrule'][0]['ruleid']
+        @another_sg['egressrule'][0]['ruleid'].must_equal @client.list_security_groups['listsecuritygroupsresponse']['securitygroup'].select{|sg| sg['name']==@another_sg['name']}[0]['egressrule'][0]['ruleid']
       end
       
       after do
@@ -83,7 +83,7 @@ describe Fog::Compute::Exoscale do
     
     after do
       begin
-        @client.delete_security_group(:name => @new_sg['name'])
+        @client.delete_security_group(:name => @another_sg['name'])
       rescue Fog::Compute::Exoscale::Error => e
         if e.message == "Cannot delete group when it's in use by virtual machines"
           sleep 30
